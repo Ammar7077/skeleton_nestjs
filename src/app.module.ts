f4ef1acbba46b12883967b8e63f0f68b74bef778
Ammar7077 <ammaromari7077@gmail.com>
2024-09-02 12:25:03 +0300

    finish admin-js

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,14 +12,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     AdminJSModule,
     TypeOrmModule.forRootAsync({
       useFactory: async () => {
-        // const { DataSource } = await import('typeorm');
         return {
           type: 'postgres',
-          host: 'localhost',
-          port: 5432,
-          username: 'postgre',
-          password: 'admin',
-          database: 'postgre',
+          host: process.env.DB_HOST,
+          port: +process.env.DB_PORT,
+          username: process.env.DB_USER,
+          password: process.env.DB_PASS,
+          database: process.env.DB_NAME,
           entities: [__dirname + '/**/*.entity{.ts,.js}'],
           synchronize: true,
         };
--- src/shared/modules/adminjs.module.ts
+++ src/shared/modules/adminjs.module.ts
@@ -1,23 +1,33 @@
-import { Module } from '@nestjs/common'
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
-    import('@adminjs/nestjs').then(async ({ AdminModule }) => AdminModule.createAdminAsync({
-      useFactory: async () => {
-        // const { Database, Resource } = await import('@adminjs/typeorm');
-        // const AdminJS = (await import('adminjs')).default;
-        
-        // AdminJS.registerAdapter({ Database, Resource });
-        
-        return ({
+    (async () => {
+      const { AdminModule } = await import('@adminjs/nestjs');
+      const { AdminJS } = await import('adminjs');
+      const { Adapter, Database, Resource } = await import('@adminjs/sql');
+      AdminJS.registerAdapter({ Database, Resource });
+
+      const options = {
+        connectionString:
+          `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+        database: process.env.DB_NAME,
+      };
+      const db = await new Adapter('postgresql', options).init();
+
+      return AdminModule.createAdminAsync({
+        useFactory: async () => ({
           adminJsOptions: {
             rootPath: '/admin',
-            resources: [],
+            resources: [
+              // { resource: db.table('test') },
+              // { resource: db.table('user') },
+              //! add tables here
+            ],
           },
-        });
-      },
-    })),
-    
+        }),
+      });
+    })(),
   ],
 })
 export class AdminJSModule {  }
